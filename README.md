31bd83488c4cca5f1b2fa4c91984099666ed7dac
Michael Santos <michael.santos@gmail.com>
2022-01-10 07:45:26 -0500

    tcpexec: minimal, UCSPI inetd
    Rust version of https://github.com/msantos/tcpexec.

---
+++ README.md
@@ -0,0 +1,91 @@
+# SYNOPSIS
+
+tcpexec *IPADDR*:*PORT* *COMMAND* *...*
+
+# DESCRIPTION
+
+[tcpexec](https://github.com/msantos/tcpexec): a minimal,
+[UCSPI](https://jdebp.uk/FGA/UCSPI.html) inetd
+
+`tcpexec` attaches the standard input and output of a command to a
+TCP socket:
+
+* exec(3): data is not proxied via an intermediary process
+
+* `SO_REUSEPORT`: multiple processes concurrently listen and accept data
+  on the same port
+
+# EXAMPLES
+
+## echo server
+
+```
+$ tcpexec [::1]:9090 env
+
+$ tcpexec [::]:9090 env
+```
+
+## Supervised using daemontools
+
+An echo server allowing 3 concurrent connections:
+
+    service/
+    ├── echo1
+    │   └── run
+    ├── echo2
+    │   └── run
+    └── echo3
+        └── run
+
+*  service/echo1/run
+
+```
+#!/bin/sh
+
+exec tcpexec [::1]:9090 cat
+```
+
+* service/echo2/run
+
+```
+#!/bin/sh
+
+exec tcpexec [::1]:9090 cat
+```
+
+* service/echo3/run
+
+```
+#!/bin/sh
+
+exec tcpexec [::1]:9090 cat
+```
+
+Then run:
+
+    svscan service
+
+# Build
+
+    cargo build
+
+# OPTIONS
+
+None
+
+# ENVIRONMENT VARIABLES
+
+PROTO
+: protocol, always set to TCP
+
+TCPREMOTEIP
+: source IPv4 or IPv6 address
+
+TCPREMOTEPORT
+: source port
+
+TCPLOCALIP
+: destination IPv4 or IPv6 address
+
+TCPLOCALPORT
+: destination port
---
+++ src/main.rs
@@ -0,0 +1,98 @@
+use std::env;
+use std::ffi::CString;
+use std::fmt::Write;
+use std::net::{SocketAddr, TcpListener};
+use std::os::unix::io::IntoRawFd;
+
+use socket2::{Domain, Socket, Type};
+
+use nix::unistd::{dup2, execvp};
+
+const PROGNAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+const STDIN_FILENO: i32 = 0;
+const STDOUT_FILENO: i32 = 1;
+const SOMAXCONN: i32 = 128;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() < 2 {
+        eprintln!(
+            r#"{} {}
+usage: <IPADDR>:<PORT> <COMMAND> <...>
+"#,
+            PROGNAME, VERSION,
+        );
+        std::process::exit(1);
+    }
+
+    let argv: Vec<_> = args
+        .iter()
+        .map(|arg| CString::new(arg.as_str()).unwrap())
+        .skip(1)
+        .collect();
+
+    let laddr = args[0].to_owned().parse()?;
+    let lsock = socket(laddr)?;
+
+    let listener: TcpListener = lsock.into();
+
+    let (stream, raddr) = listener.accept()?;
+    let fd = stream.into_raw_fd();
+
+    setremoteenv(raddr)?;
+    setlocalenv(laddr)?;
+
+    dup2(fd, STDIN_FILENO)?;
+    dup2(fd, STDOUT_FILENO)?;
+
+    execvp(&argv[0], &argv)?;
+
+    unreachable!()
+}
+
+fn socket(addr: SocketAddr) -> std::io::Result<Socket> {
+    let s = Socket::new_raw(Domain::IPV6, Type::STREAM, None)?;
+
+    s.set_only_v6(false)?;
+    s.set_reuse_address(true)?;
+    s.set_reuse_port(true)?;
+    s.bind(&addr.into())?;
+    s.listen(SOMAXCONN)?;
+
+    Ok(s)
+}
+
+fn setremoteenv(addr: SocketAddr) -> Result<(), std::fmt::Error> {
+    let mut raddr = String::new();
+    let mut rport = String::new();
+
+    write!(&mut raddr, "{}", addr.ip())?;
+    write!(&mut rport, "{}", addr.port())?;
+
+    env::remove_var("TCPREMOTEHOST");
+    env::remove_var("TCPREMOTEINFO");
+
+    env::set_var("PROTO", "TCP");
+    env::set_var("TCPREMOTEIP", raddr);
+    env::set_var("TCPREMOTEPORT", rport);
+
+    Ok(())
+}
+
+fn setlocalenv(addr: SocketAddr) -> Result<(), std::fmt::Error> {
+    let mut laddr = String::new();
+    let mut lport = String::new();
+
+    write!(&mut laddr, "{}", addr.ip())?;
+    write!(&mut lport, "{}", addr.port())?;
+
+    env::remove_var("TCPLOCALHOST");
+
+    env::set_var("TCPLOCALIP", laddr);
+    env::set_var("TCPLOCALPORT", lport);
+
+    Ok(())
+}
